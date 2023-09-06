56542c8cef7ed009f52a4559eb3ed6294cbe2ef0
Kazukizuki-takahashi <140982011+Kazukizuki-takahashi@users.noreply.github.com>
2023-09-06 10:08:01 +0900

    Add files via upload

---
+++ app_pi_releasenote.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+from streamlit.components.v1 import html
+
+
+
+# Using the st.components.v1.html method with a specified height
+st.components.v1.html('<iframe src="https://documentcloud.adobe.com/gsuiteintegration/index.html?state=%7B%22ids%22%3A%5B%2210QciVwTtWdAkpTCwtuPRrJqQfhJ4bk3E%22%5D%2C%22action%22%3A%22open%22%2C%22userId%22%3A%22109514189089438894473%22%2C%22resourceKeys%22%3A%7B%7D%7D" width="100%" height="2000"></iframe>', height=2000)
