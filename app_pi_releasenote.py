ed6fc90648c4da4221eedb1c5dc76434082c10f9
Kazukizuki-takahashi <140982011+Kazukizuki-takahashi@users.noreply.github.com>
2023-09-06 11:03:27 +0900

    Add files via upload

--- app_pi_releasenote.py
+++ app_pi_releasenote.py
@@ -5,8 +5,28 @@ def display_release_notes():
     markdown_text = """
     # AI Assistant リリースノート
 
+    ## V1.2.0（予定）
+    - リリース日時：2023年9月〇日 16:00
+
+    ### バージョンアップの概要
+    - 校正機能のプロンプト改良
+    - その他、リリースノート掲載（バージョン情報からリンク）等軽微な修正
+
+    ### 実装されている主な機能
+    - Q&A:自由に質問
+    - Translation：英文の翻訳
+    - Proofreading：日本語の文章の校正
+    - Excel Formula Analysis：Excel関数の分析
+    - VBA Analysis：VBAコードの分析
+    - Data Analysis：ログデータ等の分析
+
+    ### 今後追加開発予定の機能
+    - 出力結果のコピー / ダウンロード機能
+    - 質問ログ出力機能（何を質問したか）
+    - ファイル読み込み機能
+
     ## V1.1.0
-    - リリース日時:2023年9月5日 16:00
+    - リリース日時：2023年9月5日 16:00
 
     ### バージョンアップの概要
     - トークン数カウント機能の追加（これに伴い、単語数・文字数カウント機能は削除）
@@ -15,11 +35,11 @@ def display_release_notes():
 
     ### 実装されている主な機能
     - Q&A:自由に質問
-    - Translation:英文の翻訳
-    - Proofreading:日本語の文章の校正
-    - Excel Formula Analysis:Excel関数の分析
-    - VBA Analysis:VBAコードの分析
-    - Data Analysis:ログデータ等の分析
+    - Translation：英文の翻訳
+    - Proofreading：日本語の文章の校正
+    - Excel Formula Analysis：Excel関数の分析
+    - VBA Analysis：VBAコードの分析
+    - Data Analysis：ログデータ等の分析
 
     ### 今後追加開発予定の機能
     - 出力結果のコピー / ダウンロード機能
@@ -27,15 +47,15 @@ def display_release_notes():
     - ファイル読み込み機能
 
     ## V1.0.0
-    - リリース日時:2023年8月29日 21:00
+    - リリース日時：2023年8月29日 21:00
 
     ### 実装されている主な機能
     - Q&A:自由に質問
-    - Translation:英文の翻訳
-    - Proofreading:日本語の文章の校正
-    - Excel Formula Analysis:Excel関数の分析
-    - VBA Analysis:VBAコードの分析
-    - Data Analysis:ログデータ等の分析
+    - Translation：英文の翻訳
+    - Proofreading：日本語の文章の校正
+    - Excel Formula Analysis：Excel関数の分析
+    - VBA Analysis：VBAコードの分析
+    - Data Analysis：ログデータ等の分析
 
     ### 実装されている補助的な機能
     - 認証機能の追加
